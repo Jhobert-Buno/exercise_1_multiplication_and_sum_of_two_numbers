cf7daadc418000e39be6cbe2629e9c7e492aa4ee
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-20 20:26:08 +0800

    create functions

--- exercise_one.py
+++ exercise_one.py
@@ -1,1 +1,17 @@
 #Exercise 1: Calculate the multiplication and sum of two numbers
+
+def multiply_and_sum_of_two_numbers (first_number,second_number):
+    #calculate the product and sum
+    product = first_number * second_number
+    sum = first_number + second_number
+
+    #if less than 1000
+    if product <=1000:
+        return product
+    
+    #if greater than 1000
+    else:
+        return sum
+        
+
+
